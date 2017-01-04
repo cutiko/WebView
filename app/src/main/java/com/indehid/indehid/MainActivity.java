61ad6424e09f843e9b7f940c62269b473630c02a
cutiko <cutiko@gmail.com>
2017-01-04 20:22:38 -0300

    first commit

---
+++ app/src/main/java/com/indehid/indehid/MainActivity.java
@@ -0,0 +1,49 @@
+package com.indehid.indehid;
+
+import android.app.ProgressDialog;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Toast;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView root;
+    private ProgressDialog progressDialog;
+    private static final String URL = "https://www.google.cl/";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        progressDialog = new ProgressDialog(this);
+        progressDialog.show();
+        progressDialog.setCancelable(true);
+        root = (WebView) findViewById(R.id.rootWv);
+        root.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
+                | View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+
+        setWebview();
+    }
+
+    protected void setWebview(){
+        root.setWebViewClient(new WebViewClient() {
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                progressDialog.dismiss();
+            }
+        });
+        root.getSettings().setJavaScriptEnabled(true);
+        root.loadUrl(URL);
+    }
+
+
+}
