e051f4e1969fbe6727066564a079cdd8b5d2c575
cutiko <cutiko@gmail.com>
2017-01-05 09:45:37 -0300

    REFACTOR: methods and inner classes. ADD: correct display of progress

--- app/src/main/java/com/indehid/indehid/MainActivity.java
+++ app/src/main/java/com/indehid/indehid/MainActivity.java
@@ -7,41 +7,54 @@ import android.view.View;
 import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
-import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity {
 
     private WebView root;
     private ProgressDialog progressDialog;
-    private static final String URL = "https://www.youtube.com/";
+    private static final String URL = "http://stackoverflow.com/";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        progressDialog = new ProgressDialog(this);
-        progressDialog.show();
-        progressDialog.setCancelable(true);
         root = (WebView) findViewById(R.id.rootWv);
+        setInmersive();
+        setDialog();
+        setWebview();
+    }
+
+    private void setInmersive(){
         root.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
                 | View.SYSTEM_UI_FLAG_FULLSCREEN
                 | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
                 | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
                 | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
                 | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
-
-        setWebview();
+        /*root.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                view.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION);
+                return false;
+            }
+        });*/
     }
 
-    protected void setWebview(){
+    private void setDialog(){
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setMax(100);
+        progressDialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+        progressDialog.setTitle("Cargando");
+        progressDialog.setCancelable(true);
+        progressDialog.show();
+    }
+
+    private void setWebview(){
         root.canGoBack();
-        root.setWebViewClient(new WebViewClient() {
-            @Override
-            public void onPageFinished(WebView view, String url) {
-                progressDialog.dismiss();
-            }
-        });
+        root.setWebViewClient(new CustomWebClient());
+        root.setWebChromeClient(new CustomChromeClient());
         root.getSettings().setJavaScriptEnabled(true);
         root.loadUrl(URL);
     }
@@ -49,5 +62,19 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public void onBackPressed() {
         root.goBack();
+    }
+
+    private class CustomWebClient extends WebViewClient {
+        @Override
+        public void onPageFinished(WebView view, String url) {
+            progressDialog.dismiss();
+        }
+    }
+
+    private class CustomChromeClient extends WebChromeClient {
+        @Override
+        public void onProgressChanged(WebView view, int newProgress) {
+            progressDialog.setProgress(newProgress);
+        }
     }
 }
